3e78349117d37d4b51a44c5a862652900ef8e60b
Iaroslav Titov <iaroslav.titov@gmail.com>
2022-07-04 12:03:37 -0400

    Changed menu

--- Assets/!Project/Scripts/MenuScript.cs
+++ Assets/!Project/Scripts/MenuScript.cs
@@ -1,12 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public TMP_Text soundText;
+
+    private void Start()
+    {
+        SetSoundText();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void ToggleSound()
+    {
+        PlayerPrefs.SetInt("Sound", PlayerPrefs.GetInt("Sound") == 1? 0 : 1);
+
+        SetSoundText();
+    }
+
+    private void SetSoundText()
+    {
+        soundText.text = PlayerPrefs.GetInt("Sound") == 1 ? "♪" : "X";
     }
 }
